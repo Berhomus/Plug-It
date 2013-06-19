6b9afe6deb610cb701649d5445670623f4e7218c
Berhomus <borhane.bensaid@laposte.net>
2013-06-19 10:22:07 +0200

    lien cor => supp

--- Include/common/Banniere.php
+++ Include/common/Banniere.php
@@ -4,5 +4,5 @@
 	<li class="li"><a href="Index.php">SOLUTIONS</a></li>
 	<li class="li"><a href="Index.php">REFERENCES</a></li>
 	<li class="li"><a href="Index.php?page=contact">CONTACT</a></li>
-	<li class="li"><a href="Index.php">SUPPORT</a></li>
+	<li class="li"><a href="Index.php?page=support">SUPPORT</a></li>
 </ul>
