ae71887827bbf3cac98ddde568e56e9ce14cc308
Berhomus <borhane.bensaid@laposte.net>
2013-06-19 11:54:50 +0200

    css

--- Include/invite/support.php
+++ Include/invite/support.php
@@ -1,7 +1,7 @@
 <center>
 	<h2 class="titre">Téléchargez l’outil de prise en main à distance TeamViewer ci-dessous </h2>
 	<br/>
-	<a href="#"><img src="images/slide_01.jpg" style="z-index:3;"/></a>
+	<a href="#"><img src="images/slide_01.jpg" width='300px' height='75px'/></a>
 	<br/>
-	<img src="images/slide_01.jpg"/ style="border-color:#F9BD1A;" border="8"/>
+	<img src="images/slide_01.jpg"/ style="border-color:#F9BD1A; margin-top:15px;" border="8" width='600px' height='250px' />
 </center>
