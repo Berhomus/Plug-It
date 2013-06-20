fe08c4ef7baa32e441fc8b1be947a3aa46bfcd02
Berhomus <borhane.bensaid@laposte.net>
2013-06-20 15:39:31 +0200

    pihdfzueuzrufuôezafa^fa

--- Include/invite/services.php
+++ Include/invite/services.php
@@ -1,1 +1,36 @@
-<h2>Découvrez l'ensemble de nos services informatiques les plus pointus</h2>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" /> 
+		<title>Mon site version tp2</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href="/menu_assets/styles.css" rel="stylesheet" type="text/css">
+		<link href="index.css" rel="stylesheet" type="text/css"/> 
+
+	</head>
+
+	<body>
+	
+		<div class="milieu">
+
+			<h2>DÉCOUVREZ L'ENSEMBLE DE NOS SERVICES INFORMATIQUES LES PLUS POINTUS</h2>
+			
+						<?php
+				mysql_connect('localhost', 'root', '');
+				mysql_select_db ('plugit');
+				$retour = mysql_query('SELECT * FROM services ORDER BY id DESC') or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+					while ($donnees = mysql_fetch_array($retour))
+						{
+						$i=0; //délimite les colonnes
+						$j=0; //délimite les lignes
+						?>
+					
+					
+						<?php
+						}
+						?>
+					
+
+		</div>
+	</body>
+	
+</html>	
