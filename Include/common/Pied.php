36a974e5e75bbc3ef3f6713fe670224d8d9731fa
Villain Benoit <villain.benoit.dev@gmail.com>
2013-06-20 15:29:13 +0200

    bdd v2

--- Include/common/Pied.php
+++ Include/common/Pied.php
@@ -8,7 +8,7 @@
 <p class="p"><span class="span">Plug-It &copy; 2013</span>
 
 
-<table style="float:right; margin-top:-13px; margin-right:5%; height:40px;" cellspacing="1">
+<table style="float:right; margin-top:-5%; margin-right:5%; height:40px;" cellspacing="1">
 	<tr>
 		<td class="boutbout" onclick="location.href='<?php echo $x;?>'">
 			<?php
--- backup_bdd/plugit.sql
+++ backup_bdd/plugit.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.9
+-- version 3.4.10.1
 -- http://www.phpmyadmin.net
 --
--- Client: 127.0.0.1
--- Généré le : Mer 19 Juin 2013 à 10:11
--- Version du serveur: 5.1.50
--- Version de PHP: 5.3.9
+-- Client: localhost
+-- Généré le : Jeu 20 Juin 2013 à 13:28
+-- Version du serveur: 5.5.20
+-- Version de PHP: 5.3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `database_plug_it`
+-- Base de données: `plugit`
 --
 
 -- --------------------------------------------------------
@@ -28,10 +28,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(30) NOT NULL,
-  `mdp_md5` varchar(255) NOT NULL,
+  `login` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `mdp_md5` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `mdp_md5`) VALUES
+(1, 'moi', '70b783251225354e883a5bef3c011843');
 
 -- --------------------------------------------------------
 
@@ -41,12 +48,12 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 CREATE TABLE IF NOT EXISTS `references` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `image` varchar(100) NOT NULL,
-  `titre` varchar(100) NOT NULL,
-  `sous_titre` varchar(255) NOT NULL,
+  `image` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `titre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `sous_titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -56,12 +63,12 @@ CREATE TABLE IF NOT EXISTS `references` (
 
 CREATE TABLE IF NOT EXISTS `services` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` varchar(100) NOT NULL,
-  `corps` text NOT NULL,
-  `image` varchar(100) NOT NULL,
+  `titre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `corps` text COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -71,13 +78,14 @@ CREATE TABLE IF NOT EXISTS `services` (
 
 CREATE TABLE IF NOT EXISTS `solutions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` varchar(100) NOT NULL,
-  `corps` text NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `titre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `corps` text COLLATE utf8_unicode_ci NOT NULL,
+  `image_car` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image_sol` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
