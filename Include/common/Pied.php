a3adc169694bc014e431a4e7aa0bb023ea3c0768
Villain Benoit <villain.benoit.dev@gmail.com>
2013-06-20 14:33:25 +0200

    pied

--- Include/common/Pied.php
+++ Include/common/Pied.php
@@ -1,10 +1,20 @@
 <p class="p"><span class="span">Plug-It &copy; 2013</span>
 
-<?php
-	if(isset($_SESSION['id']))
-		echo '<a href="index.php?page=accueil&dc=1" style="margin-right:3%;">Deconnexion</a>';
-	else
-		echo '<a href="index.php?page=admin" style="margin-right:3%;">Administration</a>';
-?>
+<table>
+	<tr>
+		<td>
+			<?php
+				if(isset($_SESSION['id']))
+					echo '<a href="index.php?page=accueil&dc=1" style="margin-right:3%;">Deconnexion</a>';
+				else
+					echo '<a href="index.php?page=admin" style="margin-right:3%;">Administration</a>';
+			?>
+		</td>
+		<td>
+			<a href="index.php?page=accueil&sub=mentions" style="margin-right:3%;">MENTIONS LEGALES</a>
+		</td>
 
-<a href="index.php?page=accueil&sub=mentions" style="margin-right:3%;">MENTIONS LEGALES</a></p>
+	</tr>
+
+</table>
+</p>
