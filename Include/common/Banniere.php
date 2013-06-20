4abd77b8d8a91ec41d6c7c1c06cce880ae9c1f06
Villain Benoit <villain.benoit.dev@gmail.com>
2013-06-20 11:00:21 +0200

    modif menu

--- Include/common/Banniere.php
+++ Include/common/Banniere.php
@@ -1,14 +1,50 @@
 
 <div style="overflow:hidden;">
 	<a href="index.php?page=accueil"><img src="images/logotype_plug_it.png" style="float:left;margin-top:2%;margin-left:2%;"/></a>
-	<table style="float:right;margin-left:5%;" height="137px" class="menu">
+	<table style="float:right;margin-left:5%;margin:-1%;" height="137px" class="menu" cellspacing="0">
 		<tr>
-			<td onclick="location.href='Index.php?page=accueil'">Accueil</td>
-			<td onclick="location.href='Index.php?page=services'">Services</td>
-			<td onclick="location.href='Index.php?page=solutions'">Solutions</td>
-			<td onclick="location.href='Index.php?page=references'">Références</td>
-			<td onclick="location.href='Index.php?page=contact'">Contact</td>
-			<td onclick="location.href='Index.php?page=support'">Support</td>
+			<td onclick="location.href='Index.php?page=accueil'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'accueil')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?> >Accueil</td>
+			<td onclick="location.href='Index.php?page=services'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'services')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?>>Services</td>
+			<td onclick="location.href='Index.php?page=solutions'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'solutions')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?>>Solutions</td>
+			<td onclick="location.href='Index.php?page=references'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'references')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?>>Références</td>
+			<td onclick="location.href='Index.php?page=contact'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'contact')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?>>Contact</td>
+			<td onclick="location.href='Index.php?page=support'"
+			<?php
+				if(isset($_GET['page']) && $_GET['page'] == 'support')
+					echo 'class="menu_selected"';
+				else
+					echo 'class="menu_unselected"';
+			?>>Support</td>
 		</tr>
 	</table>
 </div>
