6b75d501a98f1d4781c58e1bd091fc4be77babcc
Berhomus <borhane.bensaid@laposte.net>
2013-06-20 14:34:03 +0200

    prout

--- Include/common/Pied.php
+++ Include/common/Pied.php
@@ -1,5 +1,7 @@
 <p class="p"><span class="span">Plug-It &copy; 2013</span>
 
+
+
 <?php
 	if(isset($_SESSION['id']))
 		echo '<a href="index.php?page=accueil&dc=1" style="margin-right:3%;">Deconnexion</a>';
