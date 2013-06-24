889b158d8d34ae0a8bca3cd5b1e94ce5a153318a
Berhomus <borhane.bensaid@laposte.net>
2013-06-24 11:33:13 +0200

    form ref

--- Include/admin/admin.php
+++ Include/admin/admin.php
@@ -56,7 +56,7 @@ else
 	<ul style="width:20%;margin:auto;">
 		<li><a href="">Nouveau Service</a></li>
 		<li><a href="">Nouvelle Solution</a></li>
-		<li><a href="">Nouvelle R&eacutef&eacuterence</a></li>
+		<li><a href="Index.php?page=admin_ref">Nouvelle R&eacutef&eacuterence</a></li>
 	</ul>
 <?php
 }
