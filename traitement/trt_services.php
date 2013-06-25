44a1f4377bb4d162bccf8cdb6fa50872a5538aa0
Villain Benoit <villain.benoit.dev@gmail.com>
2013-06-25 15:52:12 +0200

    changement admin service et trt

---
+++ traitement/trt_services.php
@@ -0,0 +1,138 @@
+<?php
+	
+	include("../function/upload.php");
+	
+	header( 'content-type: text/html; charset=utf-8' );
+	
+	mysql_connect('localhost', 'root', '')or die('Erreur SQL !<br />'.mysql_error());
+	mysql_select_db ('plugit')or die('Erreur SQL !<br />'.mysql_error());
+
+	if(isset($_GET['mode']))
+	{
+		switch($_GET['mode'])
+		{
+			case 'delete':
+				echo '<h2>Suppression Service</h2>';
+				if(isset($_GET['id']))
+				{
+					$rq=mysql_query("SELECT COUNT(id) as cpt FROM services WHERE id='".$_GET['id']."'")or die('Erreur SQL !<br />'.mysql_error());
+					$array=mysql_fetch_array($rq);
+					
+					if($array['cpt'])
+					{
+						mysql_query("DELETE FROM services WHERE id='".$_GET['id']."'")or die('Erreur SQL !<br />'.mysql_error());
+						echo '<h2 style="color:green;">Service Supprimé !</h2>';
+					}
+					else
+					{
+						echo '<h2 style="color:red;">Service inexistante !</h2>';
+					}
+				}
+				else
+				{
+					echo '<h2 style="color:red;">Service non spécifié !</h2>';
+				}
+			break;
+			
+			case 'modif':
+				echo '<h2>Modification Service</h2>';
+				if(isset($_GET['id']))
+				{
+					$rq=mysql_query("SELECT COUNT(id) as cpt FROM services WHERE id='".$_GET['id']."'")or die('Erreur SQL !<br />'.mysql_error());
+					$array=mysql_fetch_array($rq);
+
+					if($array['cpt'])
+					{
+						if(empty($_FILES['logoserv']['name']) or ($path = upload('../images/',100000,array('.png', '.gif', '.jpg', '.jpeg'),'logoserv')) != '')
+						{
+							$rq=mysql_query("SELECT * FROM services WHERE id='".$_GET['id']."'")or die('Erreur SQL !<br />'.mysql_error());
+							$array=mysql_fetch_array($rq);
+							
+							$titre = (!empty($_POST['nomserv'])) ? $_POST['nomserv']:$array['titre'];
+							$soustitre = (!empty($_POST['soustitre'])) ? $_POST['soustitre']:$array['subtitre'];
+							$corps = (!empty($_POST['corps'])) ? $_POST['corps']:$array['corps'];
+							$path = (isset($path)) ? $path:$array['image'];
+							
+							$titre = htmlspecialchars($titre);
+							$soustitre = htmlspecialchars($soustitre);
+							$corps = htmlspecialchars($corps);
+							
+							$titre = mysql_real_escape_string($titre);
+							$soustitre = mysql_real_escape_string($soustitre);
+							$corps = mysql_real_escape_string($corps);
+							
+							mysql_query("UPDATE ref SET image='$path', titre='$titre', subtitre='$soustitre', corps='$corps' WHERE id='".$_GET['id']."'")or die('Erreur SQL !<br />'.mysql_error());
+							echo '<h2 style="color:green;">Service Modifié !</h2>';
+						}
+						else
+						{
+							echo '
+								<form method="POST" action="../index.php?page=admin_services&id='.$_GET['id'].'">
+									<input type="hidden" name="nomcli" value="'.$_POST['nomref'].'"/>
+									<input type="hidden" name="soustitre" value="'.$_POST['soustitre'].'"/>
+									<input type="hidden" name="lien" value="'.$_POST['corps'].'"/>
+									<input type="submit" value="Retour Formulaire"/>
+								</form>
+							';
+						}
+					}
+					else
+					{
+						echo '<h2 style="color:red;">Service inexistante !</h2>';
+					}
+				}
+				else
+				{
+					echo '<h2 style="color:red;">Service non spécifié !</h2>';
+				}
+			break;
+			
+			case 'create':
+				echo '<h2>Création Service</h2>';
+				if(isset($_POST) and !empty($_POST))
+				{	
+					
+					if(($path = upload('../images/',100000,array('.png', '.gif', '.jpg', '.jpeg'),'logo')) != '')
+					{
+						$titre = htmlspecialchars($_POST['nomcli']);
+						$soustitre = htmlspecialchars($_POST['soustitre']);
+						
+						$titre = mysql_real_escape_string($titre);
+						$soustitre = mysql_real_escape_string($soustitre);
+						$corps = mysql_real_escape_string($_POST['corps']);
+						
+						mysql_query("INSERT INTO ref VALUES (Null,'$titre','$corps','$path','$soustitre',Null)")or die('Erreur SQL !<br />'.mysql_error());
+						echo '<h2 style="color:green;">Référence Créé !</h2>';
+					}
+					else
+					{
+						echo '
+								<form method="POST" action="../index.php?page=admin_services">
+									<input type="hidden" name="nomcli" value="'.$_POST['nomref'].'"/>
+									<input type="hidden" name="soustitre" value="'.$_POST['soustitre'].'"/>
+									<input type="hidden" name="lien" value="'.$_POST['corps'].'"/>
+									<input type="submit" value="Retour Formulaire"/>
+								</form>
+							';
+					}
+				}
+				else
+				{
+					echo '<h2 style="color:red;">Donnée inexistante !</h2>';
+				}
+			break;
+			
+			default:
+				echo '<h2 style="color:red;">404 Page Introuvable !</h2>';
+			break;
+		}
+	}
+	else
+	{
+		echo '<h2 style="color:red;">Mode Non spécifié !</h2>';
+	}
+	
+	mysql_close();
+	
+	echo '<center><a href="../index.php?page=services">Retour Référence</a></center>';
+?>
