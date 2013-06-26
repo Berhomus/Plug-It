562255bb24fd6efb0701c5a39f9053e2ab35cafd
Villain Benoit <villain.benoit.dev@gmail.com>
2013-06-26 11:55:57 +0200

    modif css3

--- Index.php
+++ Index.php
@@ -69,22 +69,66 @@ Name : Index.php => Plug-it
 					tooltipY: -15
 				});
 			});
+			
+			/*function getWindowHeight() {
+			var windowHeight = 0;
+			if (typeof(window.innerHeight) == 'number') {
+				windowHeight = window.innerHeight;
+			}
+			else {
+				if (document.documentElement && document.documentElement.clientHeight) {
+					windowHeight = document.documentElement.clientHeight;
+				}
+				else {
+					if (document.body && document.body.clientHeight) {
+						windowHeight = document.body.clientHeight;
+					}
+				}
+			}
+			return windowHeight;
+		}
+		function setFooter() {
+			if (document.getElementById) {
+				var windowHeight = getWindowHeight();
+				if (windowHeight > 0) {
+					var contentHeight = document.getElementById('content').offsetHeight;
+					var headHeight = document.getElementById('head').offsetHeight;
+					var footerElement = document.getElementById('footer');
+					var footerHeight  = footerElement.offsetHeight;
+					if (windowHeight - (contentHeight + footerHeight + headHeight) >= 0) {
+						footerElement.style.position = 'relative';
+						footerElement.style.top = windowHeight - (contentHeight + footerHeight + headHeight*2)+'px';
+					}
+				}
+			}
+		}
+		window.onload = function() {
+			setFooter();
+		}
+		window.onresize = function() {
+			setFooter();
+		}*/
 		</script>
 	</head>
 	<body>
-		<div class="Banniere">
+		<div class="Banniere" id="head">
 			<?php
 				INCLUDE("include\common\banniere.php");
 			?>
 		</div>
 		
-		<div class="Corps">
+		<div class="Corps" id="content"
+		<?php
+			if((isset($_GET['page']) and $_GET['page'] != 'accueil')or(isset($_GET['sub']) and $_GET['sub'] != 'main'))
+				echo 'style="padding-top:5%;"';
+		?>
+		>
 			<?php
 				INCLUDE("include\common\corps.php");
 			?>
 		</div>
 		
-		<div class="Pied">
+		<div class="Pied" id="footer">
 			<?php
 				INCLUDE("include\common\pied.php");
 			?>
